66a59b720e58edfb566e9c8ccfd43b19476e7976
Akshat <akshat2002garg@gmail.com>
2023-09-15 14:04:39 +0530

    Products Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+import {ProductsDataService} from './services/products-data.service';
+import {Router} from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Shipkart_Angular';
+  title = 'Shipkart';
+  products:any;
+
+  // constructor(private router:Router){}
+  
+  // goToPage(pageName : string):void {
+  //   // alert("Button got clicked");
+  //   this.router.navigate([`${pageName}`]);
+  // }
+
+  constructor(private productsData:ProductsDataService)
+  {
+    productsData.products().subscribe((data) =>{
+      console.warn("data",data);
+      this.products = data
+    });
+  }
 }
